f3395640ac4121a52c9b41d56d302c2f00466def
moradmess <morad.elmesoudy@gmail.com>
2020-12-21 21:20:27 +0000

    modifier la page actualite 2

--- resources/views/actualite.blade.php
+++ resources/views/actualite.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.menu')
 
 @section('content')
-
+<script type="text/javascript"> 
+document.getElementById('act').className = "active";
+</script>
 <div id="fb-root"></div>
   <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="eWLgnXfP"></script>
-  
+
 <div class="fb-page" data-href="https://www.facebook.com/zecoursier.officiel" data-tabs="timeline" data-width="500" data-height="100" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/zecoursier.officiel" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/zecoursier.officiel">ZeCoursier</a></blockquote></div>
+
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=100&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="100" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
