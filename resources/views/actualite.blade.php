b0d651ab07948d43f4b6d007e5c5f77371846c55
moradmess <morad.elmesoudy@gmail.com>
2020-12-21 22:06:51 +0000

    modifier la page actualite 14

--- resources/views/actualite.blade.php
+++ resources/views/actualite.blade.php
@@ -8,7 +8,7 @@ document.getElementById('actualite').className = "active";
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="eWLgnXfP"></script>
 
 <div class="row">
-	<div class="center-block">
+	<div class="col-sm-offset-3 col-sm-6">
 		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
 	</div>
 </div>
