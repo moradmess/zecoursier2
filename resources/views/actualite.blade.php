fff1f579d7b829798a722800323a48075c34945c
moradmess <morad.elmesoudy@gmail.com>
2020-12-21 21:48:34 +0000

    modifier la page actualite 10

--- resources/views/actualite.blade.php
+++ resources/views/actualite.blade.php
@@ -4,12 +4,11 @@
 <script type="text/javascript"> 
 document.getElementById('actualite').className = "active";
 </script>
-<div class="row">
-	<h2>hello</h2>
+<div id="fb-root"></div>
 </div>
-<!--<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="eWLgnXfP"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="eWLgnXfP"></script>
 
 <div class="row">
 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
-</div>-->
+</div>
 @endsection
