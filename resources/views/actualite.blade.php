4e626ac68a150988620ab3b0dc4ade3117add426
moradmess <morad.elmesoudy@gmail.com>
2020-12-23 09:30:17 +0000

    modifier pour la taille xs

--- resources/views/actualite.blade.php
+++ resources/views/actualite.blade.php
@@ -6,10 +6,12 @@ document.getElementById('actualite').className = "active";
 </script>
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0" nonce="eWLgnXfP"></script>
-
 <div class="row">
-	<div class="col-md-offset-3 col-md-9 col-sm-offset-2 col-xs-12">
+	<div id="publication" class="col-md-offset-3 col-sm-offset-2 hidden-xs">
 		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
+	</div>
+	<div id="publication" class="visible-xs">
+		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="300" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
 	</div>
 </div>
 @endsection
