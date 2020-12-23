6fdc14cc39a8a319001ca56a268153e32a45887f
moradmess <morad.elmesoudy@gmail.com>
2020-12-23 09:41:18 +0000

    pousser la pub a gauche

--- resources/views/actualite.blade.php
+++ resources/views/actualite.blade.php
@@ -10,7 +10,7 @@ document.getElementById('actualite').className = "active";
 	<div id="publication" class="col-md-offset-3 col-sm-offset-2 hidden-xs">
 		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
 	</div>
-	<div id="publication" class="visible-xs">
+	<div style="margin-left: 5px" class="visible-xs">
 		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fzecoursier.officiel&tabs=timeline&width=350&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="350" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>	
 	</div>
 </div>
